7763e345002990512026b09de5474fa5106de2b2
Shubhamm1211 <146850408+Shubhamm1211@users.noreply.github.com>
2023-11-02 17:28:45 +0530

    Time: 11 ms (43.37%), Space: 6.2 MB (60.07%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        else{
+            int temp=x;
+            long long int rev=0;
+            while(x>0){
+                rev=rev*10 + x%10;
+                x=x/10;
+            }
+            if(rev==temp)return true;
+            else return false;
+        }
+    }
+};
