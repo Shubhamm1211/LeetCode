85bf60bf9b9b7bbf810cdc3806b066883ec526d5
Shubham Agrawal <146850408+Shubhamm1211@users.noreply.github.com>
2024-04-07 13:36:20 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (20.42%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    bool checkValidString(string s) {
+        stack<int> lefts, stars;
+        int n = s.size();
+        for (int i = 0; i < n; ++i) {
+            if (s[i] == '(')
+                lefts.push(i);
+            else if (s[i] == '*')
+                stars.push(i);
+            else {
+                if (!lefts.empty())
+                    lefts.pop();
+                else if (!stars.empty())
+                    stars.pop();
+                else
+                    return false;
+            }
+        }
+        while (!lefts.empty() && !stars.empty()) {
+            if (lefts.top() > stars.top())
+                return false;
+            lefts.pop();
+            stars.pop();
+        }
+        return lefts.empty();
+    }
+};
+
+
