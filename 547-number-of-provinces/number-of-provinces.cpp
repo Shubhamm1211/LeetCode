a5a237ab69163838db5bab87ed38f1397864b19a
Shubham Agrawal <146850408+Shubhamm1211@users.noreply.github.com>
2024-04-28 13:40:34 +0530

    Time: 25 ms (28.58%) | Memory: 18 MB (74.54%) - LeetSync

---
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void dfs(int node, vector<vector<int>>& isConnected, vector<int>& vis) {
+        vis[node] = 1;
+        for (int i = 0; i < isConnected.size(); ++i) {
+            if (isConnected[node][i] && !vis[i]) {
+                dfs(i, isConnected, vis);
+            }
+        }
+    }
+    
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        vector<int> vis(n, 0);
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            if (!vis[i]) {
+                dfs(i, isConnected, vis);
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
