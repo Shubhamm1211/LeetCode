9ea048f366f39258605c59be875b83b2979c99f5
Shubham Agrawal <146850408+Shubhamm1211@users.noreply.github.com>
2024-04-28 13:40:49 +0530

    Time: 8 ms (99.78%) | Memory: 18.1 MB (74.54%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     void dfs(int node, vector<vector<int>>& isConnected, vector<int>& vis) {
         vis[node] = 1;
-        for (int i = 0; i < isConnected.size(); ++i) {
+        for (int i = 0; i < isConnected.size(); i++) {
             if (isConnected[node][i] && !vis[i]) {
                 dfs(i, isConnected, vis);
             }
@@ -13,7 +13,7 @@ public:
         int n = isConnected.size();
         vector<int> vis(n, 0);
         int ans = 0;
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; i++) {
             if (!vis[i]) {
                 dfs(i, isConnected, vis);
                 ans++;
