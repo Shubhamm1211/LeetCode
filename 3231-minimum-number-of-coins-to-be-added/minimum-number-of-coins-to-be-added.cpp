ad6ad23857be399d423661d2d1cc8ad89cbd7cf6
Shubham Agrawal <146850408+Shubhamm1211@users.noreply.github.com>
2024-06-14 00:15:51 +0530

    Time: 97 ms (72.85%) | Memory: 80.5 MB (38.23%) - LeetSync

---
+++ 3231-minimum-number-of-coins-to-be-added/minimum-number-of-coins-to-be-added.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minimumAddedCoins(vector<int>& coins, int target) {
+        sort(coins.begin(),coins.end());
+        int maxsum=0;
+        int i=0;
+        int ans=0;
+        while(maxsum<target){
+            if(i<coins.size() and coins[i]<=maxsum+1){
+                maxsum=maxsum+coins[i];
+                i++;
+            }
+            else{
+                ans++;
+                int t=maxsum+1;
+                maxsum=maxsum+t;
+            }
+        }
+        return ans;
+        
+        return ans;
+       
+    }
+};
