a8c09348066bec156027900e6f3669def6fb9464
Shubham Agrawal <146850408+Shubhamm1211@users.noreply.github.com>
2024-06-13 18:49:24 +0530

    Time: 243 ms (5.04%) | Memory: 403.3 MB (5.44%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -4,17 +4,18 @@ public:
         if(i<0)return j+1;
         if(j<0)return i+1;
         if(dp[i][j]!=-1)return dp[i][j];
-        if(w1[i]==w2[j]){
-            return dp[i][j]=solve(w1,w2,i-1,j-1,dp);
-        }
+        if(w1[i]==w2[j])return solve(w1,w2,i-1,j-1,dp);
         else{
-            return dp[i][j]=1+min(solve(w1,w2,i-1,j,dp),min(solve(w1,w2,i,j-1,dp),solve(w1,w2,i-1,j-1,dp)));
+            int a=solve(w1,w2,i-1,j,dp);
+            int b=solve(w1,w2,i-1,j-1,dp);
+            int c=solve(w1,w2,i,j-1,dp);
+            return dp[i][j]=1+min(a,min(b,c));
         }
     }
     int minDistance(string word1, string word2) {
         int n=word1.size();
         int m=word2.size();
-        vector<vector<int>>dp(n,vector<int>(m+1,-1));
+        vector<vector<int>>dp(n,vector<int>(m,-1));
         return solve(word1,word2,n-1,m-1,dp);
     }
 };
