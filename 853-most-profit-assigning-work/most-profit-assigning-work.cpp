03ed25f7187818901a708fb5b9a878b09e1c38bd
Shubham Agrawal <146850408+Shubhamm1211@users.noreply.github.com>
2024-06-18 13:10:10 +0530

    Time: 613 ms (14.56%) | Memory: 40.7 MB (78.74%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.cpp
+++ 853-most-profit-assigning-work/most-profit-assigning-work.cpp
@@ -1,22 +1,12 @@
 class Solution {
 public:
     int maxProfitAssignment(vector<int>& diff, vector<int>& profit, vector<int>& worker) {
-        unordered_map<int,int>mp;
         vector<pair<int,int>>v;
         for(int i=0;i<diff.size();i++){
             v.push_back({profit[i],diff[i]});
         }
         sort(v.begin(),v.end(),greater<pair<int,int>>());
         int ans=0;
-        // for(auto x:v){
-        //     cout<<x.first<<" "<<x.second<<endl;
-        //     if(x.second<=worker[i]){
-        //     cout<<"Ans= "<<ans<<" ";;
-        //     ans=ans+x.first;
-        //     continue;
-        //     }
-        //     i++;
-        // }
         for(int i=0;i<worker.size();i++){
             for(auto x:v){
                 if(worker[i]>=x.second){
