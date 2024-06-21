682911bba6254f177f1773d5da1d818f5f41568d
Shubham Agrawal <146850408+Shubhamm1211@users.noreply.github.com>
2024-06-21 20:56:37 +0530

    Time: 7 ms (16.59%) | Memory: 13.5 MB (37.13%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -3,16 +3,16 @@ public:
     int search(vector<int>& nums, int target) {
         int low=0,high=nums.size()-1;
         while(low<=high){
-            int mid=(low+high)/2;
+            int mid=(high+low)/2;
             if(nums[mid]==target)return mid;
 
-            if(nums[low]<=nums[mid]){
+            if(nums[mid]>=nums[low]){
                 if(nums[low]<=target and target<nums[mid]){
-                high=mid-1;
+                    high=mid-1;
                 }
                 else low=mid+1;
             }
-            else {
+            else{
                 if(nums[mid]<target and target<=nums[high]){
                     low=mid+1;
                 }
@@ -20,5 +20,6 @@ public:
             }
         }
         return -1;
+
     }
 };
