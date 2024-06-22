4d2ff0b62138e9a9311d91fa89917a93949df42d
Shubham Agrawal <146850408+Shubhamm1211@users.noreply.github.com>
2024-06-23 00:18:15 +0530

    Time: 180 ms (17.98%) | Memory: 87 MB (35.02%) - LeetSync

---
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int check(int day,vector<int>&p){
+        int sum=0;
+        for(int i=0;i<p.size();i++){
+            sum=sum+ceil((double)p[i]/(double)day) ;
+        }
+        return sum;
+    }
+    int minimizedMaximum(int h, vector<int>& p) {
+        int maxi=*max_element(p.begin(),p.end());
+        int n=p.size(),mini=-1;
+        int low=1,high=maxi;
+        while(low<high){
+            int mid=low+((high-low)/2);
+            int c=check(mid,p);
+            if(c>h) low=mid+1;
+            else high=mid;
+        }
+        return low;
+    }
+};
