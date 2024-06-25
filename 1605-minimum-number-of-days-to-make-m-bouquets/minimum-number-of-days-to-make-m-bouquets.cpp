3f9df88d2a094aa48304c9e19556f4baa76e2695
Shubham Agrawal <146850408+Shubhamm1211@users.noreply.github.com>
2024-06-25 23:16:48 +0530

    Time: 124 ms (36.83%) | Memory: 68.8 MB (99.74%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
     bool solve(vector<int>&v,int m,int k,long long int day){
-        int ans=0;
+        int ans=0,cnt=0;
         for(int i=0;i<v.size();i++){
-            int cnt=0;
-            while(i<v.size() and cnt<k and v[i]<=day){
+            if(v[i]<=day){
                 cnt++;
-                i++;
             }
-            if(cnt==k){
-                ans++;
-                i--;
+            else{
+                ans=ans+cnt/k ;
+                cnt=0;
             }
-            if(ans>=m)return true;
         }
+        ans=ans+cnt/k;
+        if(ans>=m)return true;
         return false;
     }
     int minDays(vector<int>& v, int m, int k) {
         if((long long)m*k>v.size())return -1;
         int low=*min_element(v.begin(),v.end());
         int high=*max_element(v.begin(),v.end());
-        while(low<high){
+        while(low<=high){
             long long int mid=low+(high-low)/2;
             if(solve(v,m,k,mid)){
-                high=mid;
+                high=mid-1;
             }
             else{
             low=mid+1;
@@ -32,54 +31,5 @@ public:
         return low;
     }
 };
-// class Solution {
-// public:
-//     int canMakeBouqe(vector<int>&bloomDay,int mid,int k){
-//         int bouqeCount=0;
-//         int consequtiveFlowers=0;
-
-//         for(int i=0; i<bloomDay.size();i++)
-//         {
-//             if(bloomDay[i]<=mid)
-//             {
-//                 consequtiveFlowers++;
-//             }
-//             else{
-//                 consequtiveFlowers=0;
-//             }
-//             if(consequtiveFlowers==k){
-//                 bouqeCount++;
-//                 consequtiveFlowers=0;
-//             }
-//         }
-//         return bouqeCount;
-//     }
-// public:
-//     int minDays(vector<int>& bloomDay, int m, int k) {
-//       int n = bloomDay.size();
-
-//       int startDay=0;
-//       int endDay=*max_element(bloomDay.begin(),bloomDay.end()); // max_day
-//       int minDays=-1;
-
-//       // Binary Search implementation below
-//       // O(n * log(max_day))
-//       while(startDay<=endDay) // O(log(max_day))
-//       {
-//             int mid = startDay + (endDay-startDay)/2;
-
-//             if(canMakeBouqe(bloomDay,mid,k)>=m) // O(n) time complexicity
-//             {
-//                 minDays=mid;
-//                 endDay=mid-1;
-//             }
-//             else{
-//                 startDay= mid+1;
-//             }
-//       }  
-//       return minDays;
-//     }
-// };
-
 // //tc->O(n*log(max_day))
 // //sc->O(1)
