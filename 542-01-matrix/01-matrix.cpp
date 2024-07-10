c3d66585502024d8349932e82561a50d712379b6
Shubham Agrawal <146850408+Shubhamm1211@users.noreply.github.com>
2024-07-10 20:11:22 +0530

    Time: 56 ms (49.03%) | Memory: 38 MB (22.01%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int n=mat.size();
-        int m=mat[0].size();
-        vector<vector<int>>vis(n,vector<int>(m,0));
-        vector<vector<int>>dis(n,vector<int>(m,0));
+        int RowSize=mat.size();
+        int ColSize=mat[0].size();
+        vector<vector<int>>dis(RowSize,vector<int>(ColSize,0));
+        vector<vector<int>>vis(RowSize,vector<int>(ColSize,0));
         queue<pair<pair<int,int>,int>>q;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        for(int i=0;i<RowSize;i++){
+            for(int j=0;j<ColSize;j++){
                 if(mat[i][j]==0){
-                q.push({{i,j},0});
-                vis[i][j]=1;
+                    q.push({{i,j},0});
+                    vis[i][j]=1;
+                    dis[i][j]=0;
                 }
-                else vis[i][j]=0;
             }
         }
         int dr[4]={1,-1,0,0};
-        int dc[4]={0,0,1,-1};
+        int dc[4]={0,0,-1,1};
         while(!q.empty()){
             int row=q.front().first.first;
             int col=q.front().first.second;
@@ -26,12 +26,16 @@ public:
             for(int i=0;i<4;i++){
                 int nr=row+dr[i];
                 int nc=col+dc[i];
-                if(nr>=0 && nr<n && nc>=0 && nc<m && vis[nr][nc]==0){
-                    vis[nr][nc]=1;
+                if(nr>=0 and nr<RowSize and nc>=0 and nc<ColSize and !vis[nr][nc]){
                     q.push({{nr,nc},step+1});
+                    vis[nr][nc]=1;
                 }
             }
+            
         }
+        
         return dis;
+
+
     }
 };
