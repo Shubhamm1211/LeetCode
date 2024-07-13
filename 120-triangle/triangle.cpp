9e05b75780ab5759181ce03131b032ba0af3ff26
Shubham Agrawal <146850408+Shubhamm1211@users.noreply.github.com>
2024-07-13 15:53:16 +0530

    Time: 4 ms (73.12%) | Memory: 11.7 MB (5.38%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    int solve(vector<vector<int>>& arr,int r,int c,vector<vector<int>>&dp){
-        int n=arr.size();
-        if(r==n-1)return arr[r][c];
-        if(dp[r][c]!=-1)return dp[r][c];
-        int down=solve(arr,r+1,c,dp);
-        int diag=solve(arr,r+1,c+1,dp);
-        return dp[r][c]=arr[r][c]+min(down,diag);
+    int solve(int i,int j,vector<vector<int>>&v,vector<vector<int>>&dp){
+        int n=v.size();
+        if(i==n-1)return v[n-1][j];
+        if(dp[i][j]!=-1)return dp[i][j];
+        int down=v[i][j]+solve(i+1,j,v,dp);
+        int dig=v[i][j]+solve(i+1,j+1,v,dp);
+        return dp[i][j]=min(down,dig);
     }
-    int minimumTotal(vector<vector<int>>& arr) {
-        int n=arr.size();
+    int minimumTotal(vector<vector<int>>& v) {
+        int n=v.size();
         vector<vector<int>>dp(n,vector<int>(n,-1));
-        return solve(arr,0,0,dp);
+        return solve(0,0,v,dp);
     }
 };
