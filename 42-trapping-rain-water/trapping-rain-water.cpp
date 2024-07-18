6d9ac7b608ba527ed4e8ad75d25ef2710f6fd3bd
Shubham Agrawal <146850408+Shubhamm1211@users.noreply.github.com>
2024-07-18 22:24:02 +0530

    Time: 8 ms (73.25%) | Memory: 23.7 MB (7.92%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
-    int trap(vector<int>& height) {
-        int n = height.size();
-        if (n == 0) return 0;
-        
-        vector<int> left(n);
-        vector<int> right(n);
-        
-       
-        left[0] = height[0];
-        for (int i = 1; i < n; i++) {
-            left[i] = max(left[i - 1], height[i]);
+    int trap(vector<int>&h) {
+        int n=h.size();
+        vector<int>r(n);
+        vector<int>l(n);
+        r[0]=h[0];
+        for(int i=1;i<n;i++){
+            r[i]=max(r[i-1],h[i]);
         }
-        
-        
-        right[n - 1] = height[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            right[i] = max(right[i + 1], height[i]);
+        l[n-1]=h[n-1];
+        for(int j=n-2;j>=0;j--){
+            l[j]=max(l[j+1],h[j]);
         }
-        
-        
-        int trappedWater = 0;
-        for (int i = 0; i < n; i++) {
-            trappedWater += min(left[i], right[i]) - height[i];
+        int ans=0;
+        for(int i=0;i<n;i++){
+            ans=ans+(min(l[i],r[i])-h[i]);
         }
-        
-        return trappedWater;
+        return ans;
     }
 };
