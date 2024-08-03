e51a1a67f7e7008ae90d83bdf2ccb9e339e56d8e
Shubham Agrawal <146850408+Shubhamm1211@users.noreply.github.com>
2024-08-03 11:55:22 +0530

    Time: 8 ms (66.96%) | Memory: 18.7 MB (28.14%) - LeetSync

--- 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.cpp
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -1,10 +1,20 @@
 class Solution {
 public:
     bool canBeEqual(vector<int>& tar, vector<int>& arr) {
-        sort(tar.begin(),tar.end());
-        sort(arr.begin(),arr.end());
-        for(int i=0;i<arr.size();i++){
-            if(tar[i]!=arr[i])return false;
+        // sort(tar.begin(),tar.end());
+        // sort(arr.begin(),arr.end());
+        // for(int i=0;i<arr.size();i++){
+        //     if(tar[i]!=arr[i])return false;
+        // }
+        // return true;
+        priority_queue< int , vector<int> , greater<int> >pq1(tar.begin(),tar.end());
+        priority_queue< int , vector<int> , greater<int> >pq2(arr.begin(),arr.end());
+        while(!pq1.empty() and !pq2.empty()){
+            if(pq1.top()!=pq2.top()){
+                return false;
+            }
+            pq1.pop();
+            pq2.pop();
         }
         return true;
     }
