bf9b284d4a5d756cd246d668edc101b31f181b6c
Shubham Agrawal <146850408+Shubhamm1211@users.noreply.github.com>
2024-10-08 11:58:07 +0530

    Time: 102 ms (66.21%) | Memory: 32.6 MB (79.33%) - LeetSync

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     int minSwaps(string s) {
-        int n=0;
-        
+        int n = 0;
         for(auto ch : s) {
-            if(ch=='[')n++;
-            else if(n!=0) n--;
+            if(ch == '[') n++;
+            else if(n != 0) n--;
         }
-        
-        return (n+1)/2;
+        return (n + 1) / 2;
     }
 };
