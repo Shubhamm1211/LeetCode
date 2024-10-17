a4d629661c36e40a8a500cd5f2715211a0c078ce
Shubham Agrawal <146850408+Shubhamm1211@users.noreply.github.com>
2024-10-17 12:57:38 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (12.29%) - LeetSync

--- 670-maximum-swap/maximum-swap.cpp
+++ 670-maximum-swap/maximum-swap.cpp
@@ -4,9 +4,8 @@ public:
     int ans = 0 ;
     int maximumSwap(int num) {
         s = to_string(num);
-        
-        for(int i = 0 ;i < s.size(); i++){
-            for(int j = 0 ;j < s.size(); j++){
+        for(int i = 0; i < s.size(); i++){
+            for(int j = 0; j < s.size(); j++){
                 swap(s[i],s[j]);
                 if(ans < stoi(s)){
                     ans = stoi(s);
