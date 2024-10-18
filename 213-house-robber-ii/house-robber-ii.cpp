0590e6f0f3c3adbd462353b13a36bff05fb51b63
Shubham Agrawal <146850408+Shubhamm1211@users.noreply.github.com>
2024-10-18 21:21:53 +0530

    Time: 2 ms (41.76%) | Memory: 9.8 MB (35.25%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
-    int solve(vector<int>nums,int ind,vector<int>&dp){
-        if(ind==0)return nums[0];
-        if(ind<0)return 0;
-        if(dp[ind]!=-1)return dp[ind];
-        int pick=nums[ind]+solve(nums,ind-2,dp);
-        int notpick=solve(nums,ind-1,dp);
-        return dp[ind]=max(pick,notpick);
+    int solve(vector <int> &v, int ind, vector <int> &dp){
+        if(ind == 0){
+            return v[ind];
+        }
+
+        if(ind < 0) return 0;
+
+        if(dp[ind] != -1) return dp[ind];
+
+        int pick = v[ind] + solve(v, ind - 2, dp);
+        int notpick = solve(v,ind - 1, dp);
+        
+        return dp[ind] = max(pick, notpick);
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        if(n==1)return nums[0];
-        vector<int>nums1(nums.begin(),nums.end()-1);
-        vector<int>nums2(nums.begin()+1,nums.end());
-        int n1=nums1.size();
-        vector<int>dp1(n1,-1),dp2(n1,-1);
-        int s1=solve(nums1,n1-1,dp1);
-        int n2=nums2.size();
-        int s2=solve(nums2,n2-1,dp2);
-        return max(s1,s2);
+        if (n == 1) return nums[0];
+
+        vector <int> v1(nums.begin(),nums.end() - 1);
+        vector <int> v2(nums.begin() + 1, nums.end());
+
+        int n1 = v1.size(), n2 = v2.size();
+        vector <int> dp1(n1,-1), dp2(n2,-1);
+        int a1 = solve(v1, n1 - 1, dp1);
+        int a2 = solve(v2, n2 - 1, dp2);
+        return max(a1,a2);
     }
 };
