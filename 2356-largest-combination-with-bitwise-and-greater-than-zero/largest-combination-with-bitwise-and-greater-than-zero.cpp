e3147ddc334d95f6dd764624144d84a4f034bf1a
Shubham Agrawal <146850408+Shubhamm1211@users.noreply.github.com>
2024-10-18 16:40:09 +0530

    Time: 27 ms (100.00%) | Memory: 60.3 MB (31.52%) - LeetSync

--- 2356-largest-combination-with-bitwise-and-greater-than-zero/largest-combination-with-bitwise-and-greater-than-zero.cpp
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/largest-combination-with-bitwise-and-greater-than-zero.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int largestCombination(vector<int>& nums) {
         vector<int> bits(32);  
-        for (int num : nums) {
-            for (int i = 0; i < 32; i++) {
-                if (num & (1 << i)) {
+        for(auto x : nums){
+            for(int i = 0; i < 31; i++){
+                if(x & (1 << i)){
                     bits[i]++;
                 }
             }
