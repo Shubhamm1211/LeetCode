ac72c00a0520b831b3c50e5f6cceb7874b8129d2
Shubham Agrawal <146850408+Shubhamm1211@users.noreply.github.com>
2024-10-18 23:42:30 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (6.64%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
-    int solve(vector<vector<int>>&arr,int row,int col,vector<vector<int>>&dp){
-        if(row>=0 && col>=0 && arr[row][col]==1)return 0;
-        if(row<0 || col<0)return 0;
-        if(row==0 && col==0)return 1;
-        if(dp[row][col]!=-1)return dp[row][col];
-        int up=solve(arr,row-1,col,dp);
-        int left=solve(arr,row,col-1,dp);
-        return dp[row][col]=up+left;
+    int solve(int row, int col, vector <vector<int>> &mat,vector <vector<int>> &dp){
+        if(row == 0 and col == 0){
+            if(mat[row][col] == 1){
+                return 0;
+            }
+            else return 1;
+        }
+        if(row < 0 or col < 0){
+            return 0;
+        }
+        if(mat[row][col] == 1){
+            return 0;
+        }
+        if(dp[row][col] != -1) return dp[row][col];
+        int up = solve(row - 1, col, mat,dp);
+        int left = solve(row, col - 1, mat,dp);
+
+        return dp[row][col] = up + left;
     }
-    int uniquePathsWithObstacles(vector<vector<int>>&arr) {
-        int n=arr.size();
-        int m=arr[0].size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        return solve(arr,n-1,m-1,dp);
+    int uniquePathsWithObstacles(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+        vector <vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+        return solve(n - 1, m - 1, mat,dp);
     }
 };
