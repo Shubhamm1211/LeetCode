4f1eddbf9cac405f91920a738c3bdfa23ad520b2
Shubham Agrawal <146850408+Shubhamm1211@users.noreply.github.com>
2024-10-21 15:38:08 +0530

    Time: 6 ms (37.34%) | Memory: 12.7 MB (90.23%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    void solve(int ind,int tar,vector<vector<int>>&ans,vector<int>&ds,vector<int>&can){
-            if(tar==0){
-                ans.push_back(ds);
-                return;
-            }
-            for(int i=ind;i<can.size();i++){
-                if(i>ind and can[i]==can[i-1])continue;
-                if(can[i]>tar)break;
-                ds.push_back(can[i]);
-                solve(i+1,tar-can[i],ans,ds,can);
-                ds.pop_back();
-            }
+    void solve(vector<vector<int>> &ans, vector<int> &v, int t, vector<int>&ds, int ind){
+        if(t == 0){
+            ans.push_back(ds);
+            return;
+        }
+        if(t < 0) return ;
+        for(int j = ind; j < v.size(); j++){
+            if (j > ind && v[j] == v[j - 1]) continue;
+            ds.push_back(v[j]);
+            solve(ans,v, t - v[j], ds, j + 1);
+            ds.pop_back();
+        } 
     }
-    vector<vector<int>> combinationSum2(vector<int>& can, int tar) {
-        sort(can.begin(),can.end());
-        vector<int>ds;
-        vector<vector<int>>ans;
-        solve(0,tar,ans,ds,can);
+    vector<vector<int>> combinationSum2(vector<int>& v, int t) {
+        vector<vector<int>> ans;
+        vector <int> ds;
+        sort(begin(v),end(v));
+        solve(ans,v,t,ds,0);
         return ans;
     }
 };
