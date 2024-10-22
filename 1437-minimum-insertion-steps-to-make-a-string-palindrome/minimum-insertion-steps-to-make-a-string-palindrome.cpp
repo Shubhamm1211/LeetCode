2638dc43c4dcd75a5e77049776c3bae58ef4e78f
Shubham Agrawal <146850408+Shubhamm1211@users.noreply.github.com>
2024-10-23 00:23:41 +0530

    Time: 64 ms (6.44%) | Memory: 30.1 MB (29.99%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
-    int solve(string &s1,string &s2,int ind1,int ind2,vector<vector<int>>&dp){
-        if(ind1<0 || ind2<0)return 0;
-        if(dp[ind1][ind2]!=-1)return dp[ind1][ind2];
-        if(s1[ind1]==s2[ind2])return 1+solve(s1,s2,ind1-1,ind2-1,dp);
-        return dp[ind1][ind2]=max(solve(s1,s2,ind1-1,ind2,dp),solve(s1,s2,ind1,ind2-1,dp));
+    int solve(int ind1, int ind2, string &s1, string &s2, vector <vector<int>> &dp){
+        if(ind1 < 0 or ind2 < 0){
+            return 0;
+        }
+        if(dp[ind1][ind2] != -1) return dp[ind1][ind2];
+        if(s1[ind1] == s2[ind2]){
+            return 1 + solve(ind1 - 1, ind2 - 1, s1, s2,dp);
+        }
+        int op1 = solve(ind1 - 1, ind2, s1, s2, dp);
+        int op2 = solve(ind1, ind2 - 1, s1, s2, dp);
+        return dp[ind1][ind2] = max(op1,op2);
     }
     int minInsertions(string s) {
-        string s2=s;
+        string s2 = s;
         reverse(s2.begin(),s2.end());
-        int n=s.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        return n-solve(s,s2,n-1,n-1,dp);
+        int n = s.size();
+        vector <vector<int>> dp (n,vector<int>(n,-1));
+        return n - solve(n-1, n-1, s, s2, dp);
     }
 };
