29213a2f9b130b31b7635053c07e0a5bc0c452c3
Shubham Agrawal <146850408+Shubhamm1211@users.noreply.github.com>
2024-10-23 14:04:50 +0530

    Time: 67 ms (7.47%) | Memory: 82.9 MB (7.04%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
-    int solve(string w1,string w2,int i,int j,vector<vector<int>>&dp){
-        if(i<0)return j+1;
-        if(j<0)return i+1;
-        if(dp[i][j]!=-1)return dp[i][j];
-        if(w1[i]==w2[j])return solve(w1,w2,i-1,j-1,dp);
+    int solve(int ind1, int ind2, string s1, string s2, vector<vector<int>> &dp){
+        if(ind1 < 0 ) return ind2 + 1;
+        if(ind2 < 0) return ind1 + 1;
+        if(dp[ind1][ind2] != -1) return dp[ind1][ind2];
+        if(s1[ind1] == s2[ind2]){
+            return dp[ind1][ind2] = solve(ind1 - 1, ind2 - 1, s1, s2,dp);
+        }
         else{
-            int a=solve(w1,w2,i-1,j,dp);
-            int b=solve(w1,w2,i-1,j-1,dp);
-            int c=solve(w1,w2,i,j-1,dp);
-            return dp[i][j]=1+min(a,min(b,c));
+            int a = solve(ind1 - 1, ind2, s1, s2,dp);
+            int b = solve(ind1, ind2 - 1, s1, s2,dp);
+            int c = solve(ind1 - 1, ind2 - 1, s1, s2,dp);
+            return dp[ind1][ind2] = 1 + min({a,b,c});
         }
     }
     int minDistance(string word1, string word2) {
-        int n=word1.size();
-        int m=word2.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return solve(word1,word2,n-1,m-1,dp);
+        int n1 = word1.size();
+        int n2 = word2.size();
+        vector<vector<int>>dp(n1 ,vector<int>(n2 ,-1));
+        return solve(n1 - 1, n2 - 1, word1, word2,dp);    
     }
 };
