1196ac1840e4de8f4d3dae94287e2a90bfa0ccf6
Shubham Agrawal <146850408+Shubhamm1211@users.noreply.github.com>
2024-12-07 16:11:26 +0530

    Time: 47 ms (29.33%) | Memory: 59.7 MB (22.28%) - LeetSync

---
+++ 1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+bool solve(int ball, vector<int>& nums, int maxi) {
+        int tot = 0;
+        for (int num : nums) {
+            int op = ceil(num / (double)ball) - 1;
+            tot += op;
+            if (tot > maxi) return false;
+        }
+        return true;
+    }
+    int minimumSize(vector<int>& nums, int maxi) {
+        int left = 1;
+        int right = 0;
+
+        for (auto num : nums) {
+            right = max(right, num);
+        }
+        while (left < right) {
+            int middle = (left + right) / 2;
+            if (solve(middle, nums, maxi)) {
+                right = middle; 
+                                 
+            } else {
+                left = middle + 1;  
+                                    
+            }
+        }
+        return left;
+    }
+};
