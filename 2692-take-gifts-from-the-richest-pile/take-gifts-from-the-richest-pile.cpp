2527ce94464287509f098e57acc77e7d6c739724
Shubham Agrawal <146850408+Shubhamm1211@users.noreply.github.com>
2024-12-12 13:29:06 +0530

    Time: 26 ms (5.15%) | Memory: 13.4 MB (7.74%) - LeetSync

---
+++ 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.cpp
@@ -0,0 +1,20 @@
+#define ll long long
+class Solution {
+public:
+    long long pickGifts(vector<int>& gifts, int k) {
+        int n = gifts.size();
+        priority_queue<int> pq(begin(gifts),end(gifts));
+        for(int i = 0; i < k; i++){
+            int top = pq.top();
+            pq.pop();
+            pq.push(floor(sqrt(top * 1.0)));
+        }
+        ll sum = 0;
+        while(!pq.empty()){
+            cout << pq.top() << endl;
+            sum += pq.top();
+            pq.pop();
+        }
+        return sum;
+    }
+};
