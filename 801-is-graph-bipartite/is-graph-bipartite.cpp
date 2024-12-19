8e16b4ef8c45d6785b87bff7ef82ad56b6be4703
Shubham Agrawal <146850408+Shubhamm1211@users.noreply.github.com>
2024-12-19 16:45:27 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (29.64%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
+    bool dfs(int node, int co, vector<int> &col, vector <vector <int>> & adj){
+        col[node] = co;
+            for(auto x : adj[node]){
+                if(col[x] == -1){
+                    if(dfs(x,!co,col,adj) == false) return false;
+                }
+                else if(col[x] == col[node]) return false;
+            }
+        return true;
+    }
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector <int> col(n,-1);
+        vector <int> col(n, -1);
         for(int i = 0; i < n; i++){
             if(col[i] == -1){
-                queue<int> q;
-                q.push(i);
-                col[i] = 0;
-                while(!q.empty()){
-                    int node = q.front();
-                    q.pop();
-                    for(auto x : graph[node]){
-                        if(col[x] == -1){
-                            col[x] = !col[node];
-                            q.push(x);
-                        }
-                        else if(col[x] == col[node]) return false;
-                    }
-                }
+                if(dfs(i,0,col,graph) == false) return false;
             }
         }
-        for(auto x : col) cout << x << " ";
         return true;
     }
 };
