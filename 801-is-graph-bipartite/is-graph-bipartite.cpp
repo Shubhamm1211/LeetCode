f9bd68065531bf5ab5bc594142be3ec0cce51a15
Shubham Agrawal <146850408+Shubhamm1211@users.noreply.github.com>
2024-12-19 16:31:42 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (19.00%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     bool isBipartite(vector<vector<int>>& graph) {
-        int n=graph.size();
-        vector<int>col(n,-1);
-        for(int i=0;i<n;i++){
-            if(col[i]==-1){
-                queue<int>q;
+        int n = graph.size();
+        vector <int> col(n,-1);
+        for(int i = 0; i < n; i++){
+            if(col[i] == -1){
+                queue<int> q;
                 q.push(i);
-                col[i]=0;
+                col[i] = 0;
                 while(!q.empty()){
-                    int node=q.front();
+                    int node = q.front();
                     q.pop();
-                    for(auto adj:graph[node]){
-                        if(col[adj]==-1){
-                            col[adj]=!col[node];
-                            q.push(adj);
+                    for(auto x : graph[node]){
+                        if(col[x] == -1){
+                            col[x] = !col[node];
+                            q.push(x);
                         }
-                        else if(col[adj]==col[node])return false;
+                        else if(col[x] == col[node]) return false;
                     }
                 }
             }
         }
+        for(auto x : col) cout << x << " ";
         return true;
     }
 };
