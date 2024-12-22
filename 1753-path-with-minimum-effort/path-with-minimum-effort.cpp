8ffda12b39adbcad528024b428152c02f0f80417
Shubham Agrawal <146850408+Shubhamm1211@users.noreply.github.com>
2024-12-22 16:16:02 +0530

    Time: 43 ms (91.33%) | Memory: 24.4 MB (55.77%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
-    int minimumEffortPath(vector<vector<int>>& heights) {
-       priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,
-        greater<pair<int,pair<int,int>>>>pq;
-        pq.push({0,{0,0}});
-        int n=heights.size();
-        int m=heights[0].size();
-        int dr[4]={0,0,1,-1};
-        int dc[4]={1,-1,0,0};
-        vector<vector<int>>dist(n,vector<int>(m,1e9));
-        dist[0][0]=0;
+    int minimumEffortPath(vector<vector<int>>& v) {
+        int n = v.size();
+        int m = v[0].size();
+        vector<vector<int>> dis(n, vector<int>(m,1e8));
+        dis[0][0] = 0;
+        priority_queue< pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater <pair<int,pair<int,int>>> >pq;
+        pq.push({0,{0,0}}); // {wt,{r,c}};
+        int dr[4] = {0,0,1,-1};
+        int dc[4] = {1,-1,0,0};
         while(!pq.empty()){
-            int diff=pq.top().first;
-            int r=pq.top().second.first;
-            int c=pq.top().second.second;
-            if(r==n-1 and c==m-1)return dist[r][c];
+            int wt = pq.top().first;
+            int r = pq.top().second.first;
+            int c = pq.top().second.second;
             pq.pop();
-            for(int i=0;i<4;i++){
-                int nr=r+dr[i];
-                int nc=c+dc[i];                
-                if(nr>=0 and nr<n and nc>=0 and nc<m ){
-                    int ne=max(abs(heights[r][c]-heights[nr][nc]),diff);
-                    if(ne<dist[nr][nc]){
-                        dist[nr][nc]=ne;
-                        pq.push({ne,{nr,nc}});
+            if(r == n - 1 and c == m - 1) return dis[r][c];
+            for(int i = 0; i < 4; i++){
+                int nr = r + dr[i];
+                int nc = c + dc[i];
+                if(nr >= 0 and nr < n and nc >= 0 and nc < m){
+                    int ne = max(wt,abs(v[r][c] - v[nr][nc]));
+                    if(dis[nr][nc] > ne){
+                        dis[nr][nc] = ne;
+                        pq.push({ne, {nr,nc}});
                     }
                 }
             }
