c7becdb79ca3a859be31f3fcf0e1ea2ae9ea8f32
Shubham Agrawal <146850408+Shubhamm1211@users.noreply.github.com>
2024-12-23 22:23:58 +0530

    Time: 86 ms (51.01%) | Memory: 44.4 MB (59.13%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        priority_queue< pair<int,int>, vector<pair<int,int>>, 
-        greater<pair<int,int>> >pq; //{dist,node}
-        vector<pair<int,int>>adj[n+1];
-        vector<int>dist(n+1,1e9);
-        dist[k]=0;
-        for(auto it:times){
-            adj[it[0]].push_back({it[1],it[2]});
+        vector<vector<pair<int,int>>> adj(n + 1);
+        for(int i = 0; i < times.size(); i++){
+            int u = times[i][0];
+            int v = times[i][1];
+            int t = times[i][2];
+            adj[u].push_back({v,t});
         }
+        priority_queue< pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>> >pq;
+        //{wt,node}
+        vector<int> dis(n + 1, 1e9);
+        dis[k] = 0;
         pq.push({0,k});
         while(!pq.empty()){
-            int dis=pq.top().first;
-            int node=pq.top().second;
+            auto x = pq.top();
             pq.pop();
-            for(auto it:adj[node]){
-                int wt=it.second;
-                int adjnode=it.first;
-                if(dis+wt<dist[adjnode]){
-                    dist[adjnode]=dis+wt;
-                    pq.push({dist[adjnode],adjnode});
+            int d = x.first;
+            int node = x.second;
+            for(auto it : adj[node]){
+                if(d + it.second < dis[it.first]){
+                    dis[it.first] = d + it.second;
+                    pq.push({dis[it.first] ,it.first});
                 }
             }
         }
-        int maxi=*max_element(dist.begin()+1,dist.end());
-        if(maxi!=1e9)return maxi;
-        else return -1;
+        int ans = *max_element(begin(dis) + 1, end(dis));
+        if(ans == 1e9) return -1;
+        return ans;
     }
 };
