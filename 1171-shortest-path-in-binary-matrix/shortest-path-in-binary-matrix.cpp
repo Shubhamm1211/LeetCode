b0b12c4df100239ce897d305abf6d63280e48467
Shubham Agrawal <146850408+Shubhamm1211@users.noreply.github.com>
2024-12-23 22:09:22 +0530

    Time: 25 ms (48.53%) | Memory: 23.6 MB (76.14%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        if(grid[0][0]!=0)return -1;
-        int n=grid.size();
-        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
+        if(grid[0][0] == 1) return -1;
+        int n = grid.size();
+        priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>> > pq;
+        //{len,{row,col}}
         pq.push({1,{0,0}});
-        vector<vector<int>>dist(n,vector<int>(n,INT_MAX));
-        dist[0][0]=1;
-        int dr[8]={-1,-1,0,1,1,1,0,-1};
-        int dc[8]={0,1,1,1,0,-1,-1,-1};
+        vector<vector<int>> dis(n,vector<int>(n,1e9));
+        dis[0][0] = 1;
+        int dr[8] = {-1,1,0,1,1,-1,0,-1};
+        int dc[8] = {0,1,1,-1,0,1,-1,-1};
         while(!pq.empty()){
-            auto node=pq.top();
-            int len=node.first;
-            int row=node.second.first;
-            int col=node.second.second;
+            auto x = pq.top();
             pq.pop();
-            if(row==n-1 and col==n-1)return len;
-            for(int i=0;i<8;i++){
-                int nrow=row+dr[i];
-                int ncol=col+dc[i];
-                if(nrow>=0 and nrow<n and ncol>=0 and ncol<n and grid[nrow][ncol]==0 and 
-                dist[nrow][ncol]>1+len){
-                    dist[nrow][ncol]=1+len;
-                    pq.push({len+1,{nrow,ncol}});
+            int d = x.first;
+            int row = x.second.first;
+            int col = x.second.second;
+            for(int i = 0; i < 8; i++){
+                int nr = row + dr[i];
+                int nc = col + dc[i];
+                if(nr >= 0 and nr < n and nc >= 0 and nc < n and grid[nr][nc] == 0){
+                    if(d + 1 < dis[nr][nc]){
+                        dis[nr][nc] = d + 1;
+                        pq.push({dis[nr][nc], {nr,nc}});
+                    }
                 }
             }
         }
-        return -1;
+        if(dis[n - 1][n - 1] == 1e9) return -1;
+        return dis[n - 1][n - 1];
     }
 };
