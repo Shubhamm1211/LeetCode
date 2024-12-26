b7caf9c36c59e268cc0ee66e53c3621450cb15c7
Shubham Agrawal <146850408+Shubhamm1211@users.noreply.github.com>
2024-12-26 13:07:14 +0530

    Time: 182 ms (42.72%) | Memory: 11.5 MB (78.96%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    int solve(int ind,vector<int>&nums,int target,vector<vector<int>>&dp){
-        if(ind==0){
-            if(target==0 && nums[0]==0)return 2;
-            if(target==0 || nums[ind]==target)return 1;
+    int solve(vector <int> &nums, int ind, int tar){
+        int n = nums.size();
+        if(ind == 0){
+            if(tar == 0 and nums[0] == 0 ) return 2;
+            if(tar == 0 or nums[0] == tar) return 1;
             return 0;
         }
-        if(dp[ind][target]!=-1)return dp[ind][target];
-            int notpick=solve(ind-1,nums,target,dp);
-            int pick=0;
-            if(nums[ind]<=target){
-                pick=solve(ind-1,nums,target-nums[ind],dp);
-            }
-            return dp[ind][target]=pick+notpick;
+        int pick = 0;
+        if(nums[ind] <= tar){
+            pick = solve(nums,ind - 1, tar - nums[ind]);
+        }
+        int npick = solve(nums,ind - 1,tar);
+
+        return pick + npick;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
-        int totsum=0;
-        for(auto x:nums) totsum+=x;
-        int s2=(totsum-target)/2;
-        if(totsum-target<0 || (totsum-target)%2==1 )return 0;
-        vector<vector<int>>dp(n,vector<int>(s2+1,-1));
-        return solve(n-1,nums,s2,dp);
+        int tot = accumulate(begin(nums), end(nums), 0);
+        int req = (target + tot) / 2;
+        if((target + tot) & 1) return 0;
+        return solve(nums,n - 1, req);
     }
 };
