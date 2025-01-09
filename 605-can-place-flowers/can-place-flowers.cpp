f7859d282259f13e2b8cceb8604b48020486d8f8
Shubham Agrawal <146850408+Shubhamm1211@users.noreply.github.com>
2025-01-09 21:15:53 +0530

    Time: 0 ms (100.00%) | Memory: 24.2 MB (30.38%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.cpp
+++ 605-can-place-flowers/can-place-flowers.cpp
@@ -19,39 +19,31 @@ public:
             return tot <= (n + 1) / 2;
         }
         int fi = -1;
-        if(nums[0] == 0 and nums[1] == 0) {
-            fi = 0;
-            nums[0] = 1; 
-        }
+        if(nums[0] == 0 and nums[1] == 0) fi = 0;
         else{
             for(int i = 1; i < n - 1; i++){
-                if(nums[i] == 0 && nums[i - 1] == 0 && nums[i + 1] == 0){
+                if(nums[i] == 0 and nums[i - 1] == 0 and nums[i + 1] == 0){
                     fi = i;
-                    nums[i] = 1; 
                     break;
                 }
             }
         }
         if(fi == -1){
-            if(nums[n - 2] == 0 && nums[n - 1] == 0){
+            if(nums[n - 2] == 0 and nums[n - 1] == 0){
                 fi = n - 1;
-                nums[n-1] = 1;
             }
         }
         if(fi == -1){
             return tot == 0;
         }
         int cnt = 1;
-        for(int i = fi + 2; i < n - 1; i++){  
-            if(nums[i - 1] == 0 and nums[i] == 0 and nums[i + 1] == 0){
+        for(int i = fi + 2; i < n; i++){
+            if(i - 1 >= 0 and i + 1 < n and nums[i - 1] == 0 and nums[i] == 0 and nums[i + 1] == 0){
                 cnt++;
-                nums[i] = 1; 
-                i++;  
+                i++;
             }
         }
-        if(nums[n - 2] == 0 and nums[n - 1] == 0) {
-            cnt++;
-        }
-        return cnt >= tot;
+        if(fi != n - 1 and fi != n - 2 and nums.size() > 2 and nums[n - 2] == 0 and nums[n - 1] == 0) cnt++;
+        return cnt >= tot; 
     }
 };
