f682c50a341d1a0e3a1bdca7fd29603f67b5a15a
Shubham Agrawal <146850408+Shubhamm1211@users.noreply.github.com>
2025-01-15 15:25:19 +0530

    Time: 1 ms (9.35%) | Memory: 8 MB (64.02%) - LeetSync

---
+++ 2509-minimize-xor/minimize-xor.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int minimizeXor(int num1, int num2) {
+        int n1 = __builtin_popcount(num1);
+        int n2 = __builtin_popcount(num2);
+
+        if (n1 == n2) {
+            return num1; 
+        }
+        if (n1 < n2) {
+            int x = num1; 
+            int rem = n2 - n1; 
+            for (int i = 0; rem > 0 && i < 32; ++i) {
+                if ((x & (1 << i)) == 0) {
+                    x |= (1 << i); 
+                    rem--;      
+                }
+            }
+            return x;
+        }
+        int x = num1;
+        int rem = n1 - n2; 
+        for (int i = 0; rem > 0 && i < 32; ++i) {
+            if (x & (1 << i)) {
+                x &= ~(1 << i);
+                rem--;        
+            }
+        }
+        return x;
+    }
+};
