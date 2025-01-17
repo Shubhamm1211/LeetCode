e54fe0c7b01d8c5dc934d66e2d4db58f7563ed12
Shubham Agrawal <146850408+Shubhamm1211@users.noreply.github.com>
2025-01-17 12:59:59 +0530

    Time: 0 ms (100.00%) | Memory: 263.9 MB (48.13%) - LeetSync

--- 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.cpp
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.cpp
@@ -9,9 +9,9 @@ public:
         if(xorr != 0){
             return false;
         }
-        if(n == 2){
-            return xorr == 0;
-        }
+        // if(n == 2){
+        //     return xorr == 0;
+        // }
         int tar = d[0] ^ d.back();
         int temp = 0;
         for(int i = 1; i < n - 1; i++){
