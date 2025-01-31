8e63b0fa90d60883833baae69f380961d410b747
Shubham Agrawal <146850408+Shubhamm1211@users.noreply.github.com>
2025-02-01 01:25:18 +0530

    Time: 334 ms (37.22%) | Memory: 186.7 MB (75.37%) - LeetSync

--- 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.cpp
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -11,61 +11,58 @@
  */
 class Solution {
 public:
-    int solve(TreeNode*root, TreeNode*tar,map<TreeNode*,TreeNode*>&mp){
-        map<TreeNode*,int>vis;
-        vis[tar]=1;
-        queue<TreeNode*>q;
-        q.push(tar);
-        int ans=0;
-        while(!q.empty()){
-            int s=q.size();
-            bool f=false;
-            for(int i=0;i<s;i++){
-                auto node=q.front();
-                q.pop();
-                if(node->left and !vis[node->left]){
-                    f=true;
-                    vis[node->left]=1;
-                    q.push(node->left);
-                }
-                if(node->right and !vis[node->right]){
-                    f=true;
-                    vis[node->right]=1;
-                    q.push(node->right);
-                }
-                if(mp[node] and !vis[mp[node]]){
-                    f=true;
-                    vis[mp[node]]=1;
-                    q.push(mp[node]);
-                }
-            }
-            if(f)ans++;
-        }
-        return ans;
-    }
-    TreeNode*findparent(TreeNode*root,int start,map<TreeNode*,TreeNode*>&mp){
-        queue<TreeNode*>q;
+    int amountOfTime(TreeNode* root, int target) {
+        map <TreeNode*, TreeNode*> mp;
+        TreeNode *start;
+        queue <TreeNode*> q;
         q.push(root);
-        TreeNode*ans;
+        int ans = 0;
         while(!q.empty()){
-            auto node=q.front();
-            q.pop();
-            if(node->val==start)ans=node;
-            if(node->right){
-                mp[node->right]=node;
-                q.push(node->right);
-            }
-            if(node->left){
-                mp[node->left]=node;
-                q.push(node->left);
+            int s = q.size();
+            for(int i = 0; i < s; i++){
+                TreeNode *temp = q.front();
+                q.pop();
+                if(temp -> val == target){
+                    start = temp;
+                }
+                if(temp -> left){
+                    mp[temp -> left] = temp;
+                    q.push(temp -> left);
+                }
+                if(temp -> right){
+                    mp[temp -> right] = temp;
+                    q.push(temp -> right);
+                }
             }
         }
-        return ans;
-    }
-    int amountOfTime(TreeNode* root, int start) {
-        map<TreeNode*,TreeNode*>mp;
-        TreeNode*res=findparent(root,start,mp);
-        int ans=solve(root,res,mp);
+        queue <TreeNode*> q2;
+        q2.push(start);
+        map <TreeNode*, int> vis;
+        vis[start] = 1;
+        while(!q2.empty()){
+            int s = q2.size();
+            bool f = false;
+            for(int i = 0; i < s; i++){
+                TreeNode *temp = q2.front();
+                q2.pop();
+                if(temp -> left and !vis[temp -> left]){
+                    f = true;
+                    vis[temp -> left] = 1;
+                    q2.push(temp -> left);
+                }
+                if(temp -> right and !vis[temp -> right]){
+                    f = true;
+                    vis[temp -> right] = 1;
+                    q2.push(temp -> right);
+                }
+                if(mp[temp] and !vis[mp[temp]]){
+                    f = true;
+                    vis[mp[temp]] = 1;
+                    q2.push(mp[temp]);
+                }
+            }
+            if(f) ans++;
+        }
         return ans;
     }
 };
