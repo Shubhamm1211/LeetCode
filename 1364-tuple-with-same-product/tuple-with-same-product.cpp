c81043ea36572ade67ac568f236d666ed409a206
Shubham Agrawal <146850408+Shubhamm1211@users.noreply.github.com>
2025-02-06 13:53:07 +0530

    Time: 329 ms (42.32%) | Memory: 84.9 MB (67.71%) - LeetSync

--- 1364-tuple-with-same-product/tuple-with-same-product.cpp
+++ 1364-tuple-with-same-product/tuple-with-same-product.cpp
@@ -1,28 +1,51 @@
+// class Solution {
+// public:
+//     int tupleSameProduct(vector<int>& nums) {
+//         int n = nums.size();
+//         unordered_map<int,vector<vector<int>>> mp;
+//         for(int i = 0; i < n; i++){
+//             for(int j = i + 1; j < n; j++){
+//                 int pro = nums[i] * nums[j];
+//                 mp[pro].push_back({nums[i],nums[j],0});
+//             }
+//         }
+//         int ans = 0;
+//         for(int i = 0; i < n; i++){
+//             for(int j = i + 1; j < n; j++){
+//                 if(mp.find(nums[i] * nums[j]) != mp.end()){
+//                     for(int k = 0; k < mp[nums[i] * nums[j]].size(); k++){
+//                         vector <int> temp = mp[nums[i] * nums[j]][k];
+//                         if(temp[0] != nums[i] and temp[1] != nums[j] and !temp[2]){
+//                             ans += 4;
+//                             temp[2] = 1;
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// };
 class Solution {
 public:
     int tupleSameProduct(vector<int>& nums) {
+        unordered_map<int, int> mp;
         int n = nums.size();
-        unordered_map<int,vector<vector<int>>> mp;
-        for(int i = 0; i < n; i++){
-            for(int j = i + 1; j < n; j++){
-                int pro = nums[i] * nums[j];
-                mp[pro].push_back({nums[i],nums[j],0});
-            }
-        }
         int ans = 0;
-        for(int i = 0; i < n; i++){
-            for(int j = i + 1; j < n; j++){
-                if(mp.find(nums[i] * nums[j]) != mp.end()){
-                    for(int k = 0; k < mp[nums[i] * nums[j]].size(); k++){
-                        vector <int> temp = mp[nums[i] * nums[j]][k];
-                        if(temp[0] != nums[i] and temp[1] != nums[j] and !temp[2]){
-                            ans += 4;
-                            temp[2] = 1;
-                        }
-                    }
-                }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int pro = nums[i] * nums[j];
+                mp[pro]++;
             }
         }
+        for (auto& it : mp) {
+            int freq = it.second;
+            if (freq > 1) {
+                ans += (freq * (freq - 1) / 2) * 8; 
+            }
+        }
+
         return ans;
     }
 };
