996a184e2041ee563e55f1b5af8de08e2036f6b1
Shubham Agrawal <146850408+Shubhamm1211@users.noreply.github.com>
2025-02-11 13:24:05 +0530

    Time: 260 ms (5.85%) | Memory: 294.3 MB (5.32%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -1,10 +1,33 @@
 class Solution {
 public:
-    string removeOccurrences(string s, string part) {
-        int n = s.length();
-        while(n!=0 && s.find(part)<n){
-            s.erase(s.find(part),part.length());
+    string removeOccurrences(string s, string p) {
+        int n1 = s.size();
+        int n2 = p.size();
+        stack <char> st;
+        for(int i = 0; i < n1; i++){
+            if(s[i] == p.back()){
+                int temp = n2 - 1;
+                string bud = "";
+                while(!st.empty() and temp){
+                    bud = st.top() + bud;
+                    st.pop();
+                    temp--;
+                }
+                bud = bud + s[i];
+                cout << bud << endl;
+                if(bud != p){
+                    for(auto &x : bud){
+                        st.push(x);
+                    }
+                }
+            }
+            else st.push(s[i]);
         }
-        return s;
+        string ans = "";
+        while(!st.empty()){
+            ans = st.top() + ans;
+            st.pop();
+        }
+        return ans;
     }
 };
