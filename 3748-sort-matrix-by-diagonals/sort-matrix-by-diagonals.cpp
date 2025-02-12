6b00f7a703607ba6707ac307af748bb3b6bb3634
Shubham Agrawal <146850408+Shubhamm1211@users.noreply.github.com>
2025-02-12 13:53:03 +0530

    Time: 98 ms (5.71%) | Memory: 44.9 MB (52.12%) - LeetSync

---
+++ 3748-sort-matrix-by-diagonals/sort-matrix-by-diagonals.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    vector<vector<int>> sortMatrix(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int start = n - 2;
+        while(start >= 0){
+            int r = start;
+            int c = 0;
+            vector <int> temp;
+            while(r < n and c < n){
+                temp.push_back(grid[r][c]);
+                r++;
+                c++;
+            }
+            sort(rbegin(temp), rend(temp));
+            r = start;
+            c = 0;
+            int i = 0;
+            for(auto &x : temp) cout << x << " ";
+            cout << endl;
+            while(r < n and c < n){
+                grid[r][c] = temp[i];
+                i++;
+                r++;
+                c++;
+            }
+            start--;
+        }
+        start = n - 2;
+        while(start > 0){
+            int r = 0;
+            int c = start;
+            vector <int> temp;
+            while(r < n and c < n){
+                temp.push_back(grid[r][c]);
+                r++;
+                c++;
+            }
+            sort(begin(temp), end(temp));
+            r = 0;
+            c = start;
+            int i = 0;
+            for(auto &x : temp) cout << x << " ";
+            cout << endl;
+            while(r < n and c < n){
+                grid[r][c] = temp[i];
+                i++;
+                r++;
+                c++;
+            }
+            start--;
+        }
+        return grid;
+    }
+};
