81949028cfd4d19e518655afd867b16a3986baa4
Shubham Agrawal <146850408+Shubhamm1211@users.noreply.github.com>
2025-03-14 01:05:29 +0530

    Time: 0 ms (100.00%) | Memory: 16.3 MB (82.81%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
@@ -11,25 +11,17 @@
  */
 class Solution {
 public:
-    map <int,int> mp;
-    TreeNode *construct(vector <int> &in, vector <int> &pre, int start, int end, int &ind){
-        if(start > end) return NULL;
+    TreeNode *solve(vector <int> &pre, int &ind, int bound){
+        if(ind == pre.size() or pre[ind] > bound) return NULL;
         TreeNode *root = new TreeNode(pre[ind]);
-        int temp = mp[pre[ind]];
         ind++;
-        root -> left = construct(in,pre,start, temp - 1, ind);
-        root -> right = construct(in,pre,temp + 1, end, ind);
+        root -> left = solve(pre, ind, root -> val);
+        root -> right = solve(pre, ind, bound);
         return root;
     }
     TreeNode* bstFromPreorder(vector<int>& preorder) {
-        vector <int> in(begin(preorder), end(preorder));
-        sort(begin(in), end(in));
-        int n = in.size();
-        for(int i = 0; i < n; i++){
-            mp[in[i]] = i;
-        }
         int ind = 0;
-        TreeNode *root = construct(in,preorder,0, n - 1, ind);
-        return root;
+        int bound = INT_MAX;
+        return solve(preorder,ind,bound);
     }
 };
