39bef02e1cf772fab153dd68a1210368f2a121d3
Shubham Agrawal <146850408+Shubhamm1211@users.noreply.github.com>
2025-03-15 16:12:35 +0530

    Time: 0 ms (100.00%) | Memory: 37.4 MB (37.87%) - LeetSync

---
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    bool findTarget(TreeNode* root, int k) {
+        if (!root) return false;
+        stack<TreeNode*> leftst, rightst;
+        TreeNode *curr = root;
+
+        while (curr) {
+            leftst.push(curr);
+            curr = curr->left;
+        }
+        curr = root;
+        while (curr) {
+            rightst.push(curr);
+            curr = curr->right;
+        }
+
+        TreeNode *l = leftst.top(); leftst.pop();
+        TreeNode *r = rightst.top(); rightst.pop();
+        while (l != r) { 
+            int sum = l->val + r->val;
+            if (sum == k) return true;
+            else if (sum < k) {  
+                if (l->right) {
+                    l = l->right;
+                    while (l) {
+                        leftst.push(l);
+                        l = l->left;
+                    }
+                }
+                if (!leftst.empty()) {
+                    l = leftst.top();
+                    leftst.pop();
+                }
+            } 
+            else { 
+                if (r->left) {
+                    r = r->left;
+                    while (r) {
+                        rightst.push(r);
+                        r = r->right;
+                    }
+                }
+                if (!rightst.empty()) {
+                    r = rightst.top();
+                    rightst.pop();
+                }
+            }
+        }
+        return false;
+    }
+};
